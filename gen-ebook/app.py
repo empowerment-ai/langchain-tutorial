2302f131617d35e180f77ede4b1de38c739b6b63
Randy Michak <randy.michak@761link.net>
2023-09-20 00:04:43 -0400

    ebook updates

--- gen-ebook/app.py
+++ gen-ebook/app.py
@@ -19,7 +19,8 @@ llms = OpenAI(temperature=0.6, verbose=True, max_tokens=3500)
 # Initialize Prompt Templates
 prompt_template_title = PromptTemplate(
     input_variables=['topic'],
-    template="You are an AI Assistant who is helping an author write an ebook about book about {topic}. I want you to generate a good title for such a book"
+    template="""You are an AI Assistant who is helping an author write an ebook about book about {topic}. I want you to generate a good title for such a book.  
+    The title should be between 6 to 10 words.  The title should be in title case."""
 )
 
 title_chain = LLMChain(llm=llms, prompt=prompt_template_title, output_key="title", verbose=True)
@@ -27,8 +28,10 @@ title_chain = LLMChain(llm=llms, prompt=prompt_template_title, output_key="title
 prompt_template_chapters = PromptTemplate(
     input_variables=['title', 'topic'],
     template="""
-    I want you to create a complete outline for a book called {title} that is about {topic}.  The outlook should be as detailed as necessary.  
-    All sub topics with the chapter should be completely defined as well. the output should be in JSON format.  Only return the JSON structure.
+    I want you to act as an expert on the topic of '{topic}'. I want you to create a complete outline for a book called '{title}' for that topic'.  
+    The outline should be as detailed as possible.  
+    The book should have at least 8 chapters and each chapter should have 3-5 sub topics. These subtopics should becompletely defined as well. 
+    The output should be in JSON format.  Only return the JSON structure.
     the JSON structure should be a list of chapters.  Example JSON:
 {{
    "chapters":[
@@ -104,6 +107,21 @@ chain = SequentialChain(
 # Streamlit UI
 st.title('Generate Book')
 
+# Display a warning message about the potential cost
+st.warning("""
+⚠️ Generating a full ebook will result in a large number of requests to the language model 
+and may incur substantial costs. 
+Please proceed with caution.
+""")
+
+# Checkbox to confirm the user wants to proceed
+continue_box = st.checkbox("I understand and want to continue.")
+
+# If the user doesn't check the box, don't display the rest of the UI.
+if not continue_box:
+    st.stop()
+
+
 # Text input for the topic
 topic = st.text_input('Enter the topic for the book:', '')
 
@@ -113,6 +131,10 @@ style = st.text_input('Author Style for the book:', 'Ken Burns')
 
 # Text input for the file name
 file_name = st.text_input('Enter the name for the output file:', '')
+
+# Ensure the file name ends with '.docx'
+if not file_name.lower().endswith('.docx'):
+    file_name += '.docx'
 
 # Selection for the output format
 output_format = st.selectbox('Choose the output format:', ['Word'])
@@ -130,6 +152,24 @@ if st.button('Generate'):
         st.write(f"{response['title']}")
         
         parsed_chapters = json.loads(response['book_chapters']) if isinstance(response['book_chapters'], str) else response['book_chapters']
+        
+        #Generate a Table of Contents
+        doc.add_heading('Table of Contents', level=1)
+        toc = doc.add_paragraph()
+        
+        for i, chapter in enumerate(parsed_chapters['chapters']):
+            st.write(f"Chapter {i + 1} - {chapter['title']}")
+            toc.add_run(f"{i + 1} - {chapter['title']}").bold = True
+            toc.add_run('\n')
+            for j, subtopic in enumerate(chapter['subTopics']):
+                st.write(f"\t{j + 1} - {subtopic['title']}")
+                toc.add_run(f"\t{j + 1} - {subtopic['title']}")
+                toc.add_run('\n')
+
+        # Add a page break
+
+        doc.add_page_break()
+
         
         for i, chapter in enumerate(parsed_chapters['chapters']):
              # Add chapter title to Document
