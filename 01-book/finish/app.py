b83c55d20721365681e24d78773e66701aa542c2
Randy Michak <randy.michak@761link.net>
2023-09-23 23:36:48 -0400

    prompt updates

--- 01-book/finish/app.py
+++ 01-book/finish/app.py
@@ -20,7 +20,7 @@ model = st.selectbox('Select Model:', ['gpt-3.5-turbo', 'gpt-4'])
 topic = st.text_input('Enter the topic for the book:', '')
 
 # Initialize language model
-llms = ChatOpenAI(temperature=0.0, model=model)
+llms = ChatOpenAI(temperature=0.3, model=model)
 
 # Initialize Prompt Templates with updated chapter and subtopic numbers
 prompt_template_title = PromptTemplate(
@@ -35,7 +35,10 @@ title_chain = LLMChain(llm=llms, prompt=prompt_template_title, output_key="title
 prompt_template_chapters = PromptTemplate(
     input_variables=['title', 'topic'],
     template="""
-    I want you to act as an expert on the topic of '{topic}'. I want you to an outline for a book called '{title}' for that topic.
+    I want you to act as an expert on the topic of '{topic}'. I want you to generate a table of contents for a book called '{title}' for that topic.
+    I want you to make sure that each chapter is labeled like Chapter 1 - Title, Chapter 2 - Title, etc. Do not use roman numerals.  Also, do not 
+    include the introduction or conclusion of the book in the chapters.  The Introduction should be the first section of the outline.  
+    And the conclusion should be the last section of the outline. 
         
     """
 )
@@ -62,8 +65,33 @@ prompt_template_chapters_details = PromptTemplate(
     easy to understand.  
     """
 )
-
 chapter_details_chain = LLMChain(llm=llms, prompt=prompt_template_chapters_details, output_key="chapter_details")
+
+#langchain prompt for Introduction For book
+prompt_template_introduction = PromptTemplate(
+    input_variables=['title', 'topic', 'book_chapters'],
+    template="""
+    I want you to act as an expert on the topic of '{topic}'. I want you to write an introduction for the book called '{title}' for that topic. 
+    The introduction should be 3 to 5 paragraphs.  I want you to write in a conversational tone.  I want you to write in a way that is easy to understand.
+    The introduction should match the following table of contents: {book_chapters}
+        
+    """
+)
+
+intro_chain = LLMChain(llm=llms, prompt=prompt_template_introduction, output_key="introduction")
+
+#langchain prompt for Conclusion For book
+prompt_template_conclusion = PromptTemplate(
+    input_variables=['title', 'topic', 'book_chapters'],
+    template="""
+    I want you to act as an expert on the topic of '{topic}'. I want you to write a conclusion for the book called '{title}' for that topic. 
+    The conclusion should be 3 to 5 paragraphs.  I want you to write in a conversational tone.  I want you to write in a way that is easy to understand.
+    The conclusion should match the following table of contents: {book_chapters}
+        
+    """
+)
+
+conclusion_chain = LLMChain(llm=llms, prompt=prompt_template_conclusion, output_key="conclusion")
 
 # Initialize an empty string for accumulated_text outside the button callback
 accumulated_text = ""
@@ -85,8 +113,16 @@ if st.button('Generate'):
         st.write(response['book_chapters'])
         accumulated_text += "================Table Of Contents======================\n"
         accumulated_text += f"{response['book_chapters']}\n\n"
-        
         response.update(chapter_count_chain({'book_chapters': response['book_chapters']}))
+
+        # Generate the introduction
+        st.text('Generating introduction...')
+        response.update(intro_chain({'topic': topic, 'title': response['title'], 'book_chapters': response['book_chapters']}))
+        #st.subheader("Introduction")
+        #st.write(response['introduction'])
+        accumulated_text += "================Introduction======================\n"
+        accumulated_text += f"{response['introduction']}\n\n"
+
         parsed_chapter_count = json.loads(response['chapter_count'])
         num_chapters = int(parsed_chapter_count['chapters'])
         st.text(f"Number of chapters: {num_chapters}")
@@ -110,6 +146,15 @@ if st.button('Generate'):
             progress_bar.progress(progress)
             time.sleep(0.1)  # You can remove this line, it’s just for visual effect
             
+        # Generate the conclusion
+        st.text('Generating conclusion...')
+        response.update(conclusion_chain({'topic': topic, 'title': response['title'], 'book_chapters': response['book_chapters']}))
+        #st.subheader("Conclusion")
+        #st.write(response['conclusion'])
+        accumulated_text += "================Conclusion======================\n"
+        accumulated_text += f"{response['conclusion']}\n\n"
+
+    
         # Display the accumulated text in a single text area
         st.text_area('Generated Content:', accumulated_text, height=2000)
         
